2ef7e84b0e8af0e983c9533977cd8aae61fdcc75
KuanHu <hukuan2016@gmail.com>
2017-11-20 14:08:40 -0500

    Update Tweet.scala

--- src/main/scala/Ingest/Tweet.scala
+++ src/main/scala/Ingest/Tweet.scala
@@ -25,5 +25,31 @@ class Tweet {
     implicit val formatTweet = jsonFormat6(Tweet.apply)
     implicit val formatResponse = jsonFormat2(Response.apply)
   }
+  
+  object Response {
+    import spray.json._
+
+    trait IngestibleResponse extends Ingestible[Response] {
+      override def fromString(w: String): Try[Response] = {
+        println("w=" + w.parseJson.prettyPrint)
+        import TweetProtocol._
+        Try(w.parseJson.convertTo[Response])
+      }
+    }
+    implicit object IngestibleResponse extends IngestibleResponse
+  }
+
+  object Tweet {
+    import spray.json._
+
+    trait IngestibleTweet extends Ingestible[Tweet] {
+      override def fromString(w: String): Try[Tweet] = {
+        println("w=" + w.parseJson.prettyPrint)
+        import TweetProtocol._
+        Try(w.parseJson.convertTo[Tweet])
+      }
+    }
+    implicit object IngestibleTweet extends IngestibleTweet
+  }
 
 }
